8e9133a680237d1253504764bbb582614a046215
antnknvlnk <antonkonovalenko1@gmail.com>
2021-04-17 00:16:35 +0300

    Changing not null check

--- src/main/java/lesson_nine/Main.java
+++ src/main/java/lesson_nine/Main.java
@@ -9,28 +9,28 @@ public class Main {
     private static Object IOException;
 
     public static void main(String[] args) throws IOException {
-        FirstInterfaceImpl firstInterfaceImpl = new FirstInterfaceImpl();
-        SecondInterfaceImpl secondInterface = new SecondInterfaceImpl();
-
-        FirstInterface.printSum(2, 3);
-        firstInterfaceImpl.check();
-        firstInterfaceImpl.defaultPrint("Test string");
-        firstInterfaceImpl.print();
-
-        SecondInterface.toUpperCase("some string value");
-        secondInterface.check();
-        secondInterface.defaultPrint("SecondInterfaceImpl");
-        secondInterface.print();
-
-        ThirdInterface.printRandomNumber();
-
-        FourthInterface.checkNumber(12);
+//        FirstInterfaceImpl firstInterfaceImpl = new FirstInterfaceImpl();
+//        SecondInterfaceImpl secondInterface = new SecondInterfaceImpl();
+//
+//        FirstInterface.printSum(2, 3);
+//        firstInterfaceImpl.check();
+//        firstInterfaceImpl.defaultPrint("Test string");
+//        firstInterfaceImpl.print();
+//
+//        SecondInterface.toUpperCase("some string value");
+//        secondInterface.check();
+//        secondInterface.defaultPrint("SecondInterfaceImpl");
+//        secondInterface.print();
+//
+//        ThirdInterface.printRandomNumber();
+//
+//        FourthInterface.checkNumber(12);
 
         Helper helper = new Helper();
         System.out.println(helper.optionalHelper("anton@mail.ua"));
-        System.out.println(helper.optionalHelper("a a"));
+//        System.out.println(helper.optionalHelper(null));
 
-        System.out.println(helper.optionalHelper("anton @ mail.ua").orElseThrow(java.io.IOException::new));
+//        System.out.println(helper.optionalHelper("anton @ mail.ua").orElseThrow(java.io.IOException::new));
 
     }
 }
--- src/main/java/lesson_nine/utill/Helper.java
+++ src/main/java/lesson_nine/utill/Helper.java
@@ -1,19 +1,12 @@
 package lesson_nine.utill;
 
 import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Helper {
 
     public Optional<String> optionalHelper(String email) {
-        boolean found;
-        Pattern pattern = Pattern.compile("^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)" +
-                "*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$");
-        Matcher matcher = pattern.matcher(email);
-        found = matcher.matches();
-
-        if(email == null || email.isEmpty() || !found){
+        if (email == null || email.isEmpty() || !email.matches("^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)" +
+                "*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")){
             return Optional.empty();
         } else {
             return Optional.of(email);
